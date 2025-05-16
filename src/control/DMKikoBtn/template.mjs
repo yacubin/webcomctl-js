ba1eaaa1d74e286d1c8560130080a5e67edfccf5
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-05-16 23:20:58 +0300

    150525 (#162)
    * DropFile, DropBlock, DragDropToView, DMKikoView, DMBtn2, DMBtn, DebugPanel, DBC All, ConsoleContent, ConnectionStatus
    
    * DropFile, DropBlock, DragDropToView, DMKikoView, DMBtn2, DMBtn, DebugPanel, DBC All, ConsoleContent, ConnectionStatus
    
    ---------
    
    Co-authored-by: root <root@DESKTOP-0ORMFAJ.localdomain>

--- src/control/DMKikoBtn/template.mjs
+++ src/control/DMKikoBtn/template.mjs
@@ -4,20 +4,21 @@ import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
 
 const mk = new ControlMaker('DMKikoBtn', import.meta.url);
 
-const ROOT_CLASS = mk.newClassName("ROOT_CLASS");
-
 const MOON_IMG = await mk.loadSvgAsCssUrl('./moon.svg');
 const SUN_IMG = await mk.loadSvgAsCssUrl('./sun.svg');
 
+const clss = mk.newClassNameMap([
+  "ROOT_CLASS",
+]);
+
 const vars = mk.newCSSVariableMap({
   img: [ MOON_IMG,  SUN_IMG ],
+  IMAGE_BORDER_COLOR: [ 'transparent', 'transparent' ],
+  BORDER_COLOR: [ '#6a6a6a', '#6a6a6a' ],
 });
 
-const BORDER_COLOR = '#6a6a6a';
-const IMAGE_BORDER_COLOR = 'transparent';
-
 mk.newHTML('ROOT_HTML', `
-<div class="${ROOT_CLASS}">
+<div class="${clss.ROOT_CLASS}">
   <div></div>
 </div>
 `);
@@ -33,24 +34,24 @@ ${DARKMODE_SELECTOR_VALUE}
   ${vars.toString(1)};
 }
 
-.${ROOT_CLASS}
+.${clss.ROOT_CLASS}
 {
   width: 50px;
   height: 50px;
-  border: 2px solid ${BORDER_COLOR};
+  border: 2px solid ${vars.BORDER_COLOR.asVar()};
   margin: 10px;
   border-radius: 10px;
   overflow: hidden;
 }
 
-.${ROOT_CLASS} > div
+.${clss.ROOT_CLASS} > div
 {
   width: 100%;
   height: 100%;
   background-position: center;
   background-size: contain;
   background-repeat: no-repeat;
-  border: 4px solid ${IMAGE_BORDER_COLOR};
+  border: 4px solid ${vars.IMAGE_BORDER_COLOR.asVar()};
   background-image: ${vars.img.asVar()};
 }
 `);
