5200d4dd34040c79adeb89297bbd5a59d7b15a94
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-06-13 23:00:20 +0300

    GitHub icon (#178)

--- src/control/DBCLink/template.mjs
+++ src/control/DBCLink/template.mjs
@@ -5,8 +5,12 @@ const mk = new ControlMaker('DBCLink', import.meta.url);
 
 const OpS = '#85858526';
 const dbc_col = '#5063b1';
+const GitHub_IMG = await mk.loadSvgAsCssUrl('./GitHub.svg');
+const GitHub_IMG2 = await mk.loadSvgAsCssUrl('./GitHub2.svg');
 
 const vars = mk.newCSSVariableMap({
+  dbc_col_hov: [ '#1e2955', '#a0a5ba' ],
+  GitHub: [ GitHub_IMG, GitHub_IMG2 ],
 });
 
 const clss = mk.newClassNameMap([
@@ -15,7 +19,7 @@ const clss = mk.newClassNameMap([
 
 mk.newHTML('ROOT_HTML', `
   <a href="https://github.com/ykbin/dbc" class="${clss.ROOT_CLASS} notranslate" translate="no" target="_blank" draggable="false">
-    Sources code <span>(GitHub)</span>
+    <div></div><span>GitHub</span>
   </a>
 `);
 
@@ -32,12 +36,13 @@ ${DARKMODE_SELECTOR_VALUE}
 
 .${clss.ROOT_CLASS}
 {
-  display: block;
+  display: flex;
+  align-items: center;
   color: ${dbc_col};
   width: auto;
-  height: 25px;
+  height: auto;
   margin: 0 0 0 5px;
-  padding: 3px;
+  padding: 3px 5px;
   border-radius: 5px;
   font-size: 16px;
   cursor: pointer;
@@ -49,6 +54,18 @@ ${DARKMODE_SELECTOR_VALUE}
 .${clss.ROOT_CLASS}:hover
 {
   background-color: ${OpS};
+  color: ${vars.dbc_col_hov.asVar()};
+}
+
+.${clss.ROOT_CLASS} > div
+{
+  height: 30px;
+  width: 30px;
+  border: 4px solid transparent;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  background-image: ${vars.GitHub.asVar()};
 }
 
 @media (device-width < ${HEADER_MOBILE_DEVICE_WIDTH})
