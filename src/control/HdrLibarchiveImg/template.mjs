6f734d8b1d8bcadec91e64cf1a6bac28b7987fcc
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-06-20 02:12:20 +0300

    Header adding buttons

---
+++ src/control/HdrLibarchiveImg/template.mjs
@@ -0,0 +1,14 @@
+import ControlMaker from '../../lib/ControlMaker.mjs';
+import HdrBaseLogoImg from '../../comp/HdrBaseLogoImg.mjs';
+
+export async function buildComponent() {
+  const mk = new ControlMaker("HdrLibarchiveImg", import.meta.url);
+  HdrBaseLogoImg(mk, {
+    favicon: [
+      await mk.loadSvgAsCssUrl('./favicon1.svg'),
+      await mk.loadSvgAsCssUrl('./favicon2.svg'),
+    ],
+    width: 56,
+  });
+  return mk.buildComponent();
+}
