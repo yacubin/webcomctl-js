1108254c3e71a3a2c1f8bbbcce0825611d9bfa76
Yurii Yakubin <119136621+ykbin@users.noreply.github.com>
2025-06-20 21:03:09 +0100

    HdrBaseButton (#182)

--- src/control/HdrHomeButton/template.mjs
+++ src/control/HdrHomeButton/template.mjs
@@ -1,109 +1,18 @@
 import ControlMaker from '../../lib/ControlMaker.mjs';
+import HdrBaseButton from '../../comp/HdrBaseButton.mjs';
 
-import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
-import { HEADER_MOBILE_DEVICE_WIDTH } from '../../lib/WickedTheme.mjs';
-import { HEADER_FONT_SIZE } from '../../lib/WickedTheme.mjs';
-import { HEADER_FONT_COLOR } from '../../lib/WickedTheme.mjs';
-import { HEADER_FONT_FAMILY } from '../../lib/WickedTheme.mjs';
-import { HEADER_BACKGROUND_COLOR } from '../../lib/WickedTheme.mjs';
-import { HEADER_BORDER_RADIUS_HOVER } from '../../lib/WickedTheme.mjs';
-import { HEADER_COLOR_HOVER_DARK } from '../../lib/WickedTheme.mjs';
-import { HEADER_COLOR_HOVER } from '../../lib/WickedTheme.mjs';
-
-const mk = new ControlMaker('HdrHomeButton', import.meta.url);
-
-const clss = mk.newClassNameMap([
-  "ROOT_CLASS",
-]);
-
-const vars = mk.newCSSVariableMap({
-  btnBg: [ HEADER_BACKGROUND_COLOR ],
-  btnCol: [ HEADER_COLOR_HOVER, HEADER_COLOR_HOVER_DARK ],
-  image: [
-    await mk.loadSvgAsCssUrl('./home_light_hover.svg'),
-    await mk.loadSvgAsCssUrl('./home_dark_hover.svg'),
-  ],
-});
-
-const MAIN_IMG = await mk.loadSvgAsCssUrl('./home_default.svg');
-
-mk.newHTML('ROOT_HTML', `
-<a class="${clss.ROOT_CLASS} notranslate" translate="no" href="\${ENV:HOST_URL}" draggable="false">
-  <div></div>
-  <span>Home</span>
-</a>
-`);
-
-mk.newCSS('CSS', `
-:root
-{
-  ${vars.toString(0)};
-}
-
-${DARKMODE_SELECTOR_VALUE}
-{
-  ${vars.toString(1)};
-}
-
-.${clss.ROOT_CLASS}
-{
-  display: flex;
-  width: min-content;
-  height: min-content;
-  margin: 0px 5px;
-  padding-right: 5px;
-  color: ${HEADER_FONT_COLOR};
-  font-family: ${HEADER_FONT_FAMILY};
-  font-size: ${HEADER_FONT_SIZE};
-  cursor: pointer;
-  user-select: none;
-  text-decoration: none;
-  box-sizing: border-box;
-  flex-shrink: 0;
-}
-
-.${clss.ROOT_CLASS}:hover
-{
-  color: ${vars.btnCol.asVar()};
-  background-color: ${vars.btnBg.asVar()};
-  border-radius: ${HEADER_BORDER_RADIUS_HOVER};
-  transition: background-color 0.2s;
-}
-
-.${clss.ROOT_CLASS}:hover > div
-{
-  background-image: ${vars.image.asVar()};
-}
-
-.${clss.ROOT_CLASS} > div
-{
-  display: block;
-  width: 40px;
-  height: 30px;
-  border: 4px solid transparent;
-  background-image: ${MAIN_IMG};
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-position: center;
-  flex-shrink: 0;
-  box-sizing: border-box;
-}
-
-@media (device-width < ${HEADER_MOBILE_DEVICE_WIDTH})
-{
-  .${clss.ROOT_CLASS} > div
-  {
-    width: 60px;
-    height: 55px;
-  }
-  .${clss.ROOT_CLASS}
-  {
-    font-size: 60px;
-  }
-}
-`);
-
-export function buildComponent()
-{
+export async function buildComponent() {
+  const mk = new ControlMaker("HdrHomeButton", import.meta.url);
+  HdrBaseButton(mk, {
+    type: "Link",
+    url: "\${ENV:HOST_URL}",
+    anchorTarget: "_self",
+    text: "Home",
+    mainImage: await mk.loadSvgAsCssUrl("./home_default.svg"),
+    hoverImage: [
+      await mk.loadSvgAsCssUrl("./home_light_hover.svg"),
+      await mk.loadSvgAsCssUrl("./home_dark_hover.svg"),
+    ],
+  });
   return mk.buildComponent();
 }
--- src/control/HdrUploadButton/template.mjs
+++ src/control/HdrUploadButton/template.mjs
@@ -1,120 +1,16 @@
 import ControlMaker from '../../lib/ControlMaker.mjs';
+import HdrBaseButton from '../../comp/HdrBaseButton.mjs';
 
-import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
-import { HEADER_MOBILE_DEVICE_WIDTH } from '../../lib/WickedTheme.mjs';
-import { HEADER_FONT_SIZE } from '../../lib/WickedTheme.mjs';
-import { HEADER_FONT_COLOR } from '../../lib/WickedTheme.mjs';
-import { HEADER_FONT_FAMILY } from '../../lib/WickedTheme.mjs';
-import { HEADER_BACKGROUND_COLOR } from '../../lib/WickedTheme.mjs';
-import { HEADER_BORDER_RADIUS_HOVER } from '../../lib/WickedTheme.mjs';
-import { HEADER_COLOR_HOVER_DARK } from '../../lib/WickedTheme.mjs';
-import { HEADER_COLOR_HOVER } from '../../lib/WickedTheme.mjs';
-
-const mk = new ControlMaker('HdrUploadButton', import.meta.url);
-
-const clss = mk.newClassNameMap([
-  "ROOT_CLASS",
-  "HIDDEN_CLASS",
-]);
-
-const MAIN_IMG = await mk.loadSvgAsCssUrl('./search_default.svg');
-
-const vars = mk.newCSSVariableMap({
-  btncol: [HEADER_COLOR_HOVER, HEADER_COLOR_HOVER_DARK ],
-  image: [
-    await mk.loadSvgAsCssUrl('./search_light_hover.svg'),
-    await mk.loadSvgAsCssUrl('./search_dark_hover.svg'),
-  ],
-});
-
-mk.newHTML('ROOT_HTML', `
-<label class="${clss.ROOT_CLASS} ${clss.HIDDEN_CLASS} notranslate" translate="no" draggable="false">
-  <div></div>
-  <span>Upload</span>
-  <!--<input type="file">-->
-</label>
-`);
-
-mk.newHTML('CSS', `
-:root
-{
-  ${vars.toString(0)};
-}
-
-${DARKMODE_SELECTOR_VALUE}
-{
-  ${vars.toString(1)};
-}
-
-.${clss.ROOT_CLASS} > input
-{
-  display: none;
-}
-
-.${clss.HIDDEN_CLASS}
-{
-  visibility: hidden;
-}
-
-.${clss.ROOT_CLASS}
-{
-  display: flex;
-  width: min-content;
-  height: min-content;
-  margin: 0px 5px;
-  padding-right: 5px;
-  color: ${HEADER_FONT_COLOR};
-  font-size: ${HEADER_FONT_SIZE};
-  font-family: ${HEADER_FONT_FAMILY};
-  cursor: pointer;
-  text-decoration: none;
-  user-select: none;
-  box-sizing: border-box;
-  flex-shrink: 0;
-}
-
-.${clss.ROOT_CLASS} > div
-{
-  display: block;
-  width: 40px;
-  height: 30px;
-  border: 4px solid transparent;
-  background-image: ${MAIN_IMG};
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: contain;
-  flex-shrink: 0;
-  box-sizing: border-box;
-}
-
-.${clss.ROOT_CLASS}:hover 
-{
-  color: ${vars.btncol.asVar()};
-  background-color: ${HEADER_BACKGROUND_COLOR};
-  border-radius: ${HEADER_BORDER_RADIUS_HOVER};
-  transition: background-color 0.2s;
-}
-
-.${clss.ROOT_CLASS}:hover div
-{
-  background-image: ${vars.image.asVar()};
-}
-
-@media (device-width < ${HEADER_MOBILE_DEVICE_WIDTH})
-{
-  .${clss.ROOT_CLASS} > div
-  {
-    width: 60px;
-    height: 55px;
-  }
-  .${clss.ROOT_CLASS}
-  {
-    font-size: 60px;
-  }
-}
-`);
-
-export function buildComponent()
-{
+export async function buildComponent() {
+  const mk = new ControlMaker("HdrUploadButton", import.meta.url);
+  HdrBaseButton(mk, {
+    type: "FileUpload",
+    text: "Upload",
+    mainImage: await mk.loadSvgAsCssUrl("./search_default.svg"),
+    hoverImage: [
+      await mk.loadSvgAsCssUrl("./search_light_hover.svg"),
+      await mk.loadSvgAsCssUrl("./search_dark_hover.svg"),
+    ],
+  });
   return mk.buildComponent();
 }
