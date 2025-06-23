af78c3d716f6db26e6c84190d8cef842f3a2a2ac
Yurii Yakubin <119136621+ykbin@users.noreply.github.com>
2025-06-23 23:01:31 +0100

    HdrLibarchiveGitHubButton (#186)

---
+++ src/control/HdrDBCGitHubButton/template.mjs
@@ -0,0 +1,10 @@
+import ControlMaker from '../../lib/ControlMaker.mjs';
+import HdrGitHubButton from '../../comp/HdrGitHubButton/index.mjs';
+
+export async function buildComponent() {
+  const mk = new ControlMaker("HdrDBCGitHubButton", import.meta.url);
+  await HdrGitHubButton(mk, {
+    url: "https://github.com/ykbin/dbc",
+  });
+  return mk.buildComponent();
+}
