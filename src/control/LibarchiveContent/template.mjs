954cf8269dedf59483d7cb4debd175067d20cee1
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-06-27 23:25:43 +0300

    LibarchiveContent

--- src/control/LibarchiveContent/template.mjs
+++ src/control/LibarchiveContent/template.mjs
@@ -1,9 +1,6 @@
 import ControlMaker from '../../lib/ControlMaker.mjs';
 
 import { DARKMODE_SELECTOR_VALUE } from '../../lib/DarkMode.mjs';
-import { UIC_CONTENT_BACKGROUND_COLOR } from '../../lib/WickedTheme.mjs';
-import { UIC_CONTENT_BACKGROUND_COLOR_DARK } from '../../lib/WickedTheme.mjs';
-import { TOOLBAR_FONT_MONOSPACE } from '../../lib/WickedTheme.mjs';
 
 const mk = new ControlMaker('LiarchiveContent', import.meta.url);
 
