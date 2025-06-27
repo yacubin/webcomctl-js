8779b63ae0863ba97e6d92ebbbff9c40210f8e88
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-06-27 23:27:14 +0300

    LibarchiveContent

--- src/control/LibarchiveContent/index.mjs
+++ src/control/LibarchiveContent/index.mjs
@@ -1,6 +1,3 @@
-import { BaseControl, NQDOM, FileChunkLoader } from 'webnetq-js';
-import { CONTENT_CLASS, OFSLIST_CLASS, BINLIST_CLASS, TXTLIST_CLASS, SCROLL_MAIN_CLASS, SCROLL_BAR_CLASS, SCROLL_THUMB_CLASS } from 'uictmplt-loader!./template.mjs';
-
 import { BaseControl } from 'webnetq-js';
 
 export default class LibarchiveContent extends BaseControl {
