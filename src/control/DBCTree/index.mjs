2b743becdd00c17f47392e88e777f3d88b1d519c
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-07-11 02:38:06 +0300

    DBCTree_JAVA

--- src/control/DBCTree/index.mjs
+++ src/control/DBCTree/index.mjs
@@ -1,4 +1,13 @@
 import { BaseControl } from 'webnetq-js';
+import { dbc_state_expand, dbc_state_click } from 'uictmplt-loader!./template.mjs';
+
+const EXPAND_TYPE = "dbc_state_expand";
+const CLICK_TYPE = "dbc_state_click";
 
 export default class DBCTreeControl extends BaseControl {
+    _init() {
+        document.getElementByClassName(CLICK_TYPE).addEventListener("click", (event) => {
+            CLICK_TYPE.classList.remove(EXPAND_TYPE) && CLICK_TYPE.classList.add(EXPAND_TYPE);
+        })
+    }
 };
