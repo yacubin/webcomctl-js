82670e768f80241dfe3ea49745d4b0087e86914e
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-07-11 21:10:23 +0300

    DBCTree_JAVA

--- src/control/DBCTree/index.mjs
+++ src/control/DBCTree/index.mjs
@@ -4,6 +4,7 @@ import { dbc_state_expand, dbc_state_click } from 'uictmplt-loader!./template.mj
 export default class DBCTreeControl extends BaseControl {
   _init() {
     const stateElm = NQDOM.getElementByClassName(this.element, dbc_state_click);
+    console.log("Hey", stateElm)
     if (stateElm) {
       stateElm.addEventListener("click", () => {
         stateElm.classList.toggle(dbc_state_expand);
