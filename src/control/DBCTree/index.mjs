5bd0afd8117a6867baf4a78b832a62cd3c132665
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-07-11 03:09:54 +0300

    DBCTree_JAVA

--- src/control/DBCTree/index.mjs
+++ src/control/DBCTree/index.mjs
@@ -5,13 +5,12 @@ const EXPAND_TYPE = "dbc_state_expand";
 const CLICK_TYPE = "dbc_state_click";
 
 export default class DBCTreeControl extends BaseControl {
-_init() {
-  const elements = document.getElementsByClassName(CLICK_TYPE);
-  Array.from(elements).forEach(el => {
-    el.addEventListener("click", () => {
-      el.classList.remove(EXPAND_TYPE);
-      el.classList.add(EXPAND_TYPE);
-    });
-  });
+    _init() {
+        const elements = document.getElementsByClassName(CLICK_TYPE);
+        Array.from(elements).forEach(el => {
+            el.addEventListener("click", () => {
+                el.classList.toggle(EXPAND_TYPE);
+            });
+       });
     }
 };
