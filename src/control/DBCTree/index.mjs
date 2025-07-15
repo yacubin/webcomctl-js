e3cc67b11dfffebde0b87beb5bf32106176a0f37
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-07-15 21:35:08 +0300

    renaming class names

--- src/control/DBCTree/index.mjs
+++ src/control/DBCTree/index.mjs
@@ -1,13 +1,13 @@
 import { BaseControl, NQDOM } from 'webnetq-js';
-import { dbc_state_expand, dbc_state_click } from 'uictmplt-loader!./template.mjs';
+import { STATE_EXPAND, STATE_CLICK } from 'uictmplt-loader!./template.mjs';
 
 export default class DBCTreeControl extends BaseControl {
   _init() {
-    const stateElm = NQDOM.getElementByClassName(this.element, dbc_state_click);
+    const stateElm = NQDOM.getElementByClassName(this.element, STATE_CLICK);
     console.log("Hey", stateElm)
     if (stateElm) {
       stateElm.addEventListener("click", () => {
-        stateElm.classList.toggle(dbc_state_expand);
+        stateElm.classList.toggle(STATE_EXPAND);
       });
     }
   }
