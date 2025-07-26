d894f8bdf7263d0210cd474e09f298544de6b48a
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-07-26 03:31:51 +0300

    Fix DBCMessage DBCGruop DBCDocument DBCAttribute

--- src/control/DBCComment/index.mjs
+++ src/control/DBCComment/index.mjs
@@ -1,38 +1,7 @@
 import { BaseControl, NQDOM } from 'webnetq-js';
-import { dbc_view_document, dbc_view_message, dbc_view_signal, dbc_view_group, dbc_comment_root, dbc_comment_text, dbc_attributes_root, dbc_attributes_list, dbc_group_signals } from 'uictmplt-loader!./template.mjs';
+import { dbc_comment_root, dbc_comment_text } from 'uictmplt-loader!./template.mjs';
 
-const DOCUMENT_TYPE = "document";
-const MESSAGE_TYPE = "message";
-const SIGNAL_TYPE = "signal";
-const GROUP_TYPE = "group";
-
-function typeToClassName(type) {
-  switch (type) {
-  case DOCUMENT_TYPE:
-    return dbc_view_document;
-  case MESSAGE_TYPE:
-    return dbc_view_message;
-  case SIGNAL_TYPE:
-    return dbc_view_signal;
-  case GROUP_TYPE:
-    return dbc_view_group;
-  }
-}
-
-function classNameFromType(classname) {
-  switch (classname) {
-  case dbc_view_document:
-    return DOCUMENT_TYPE;
-  case dbc_view_message:
-    return MESSAGE_TYPE;
-  case dbc_view_signal:
-    return SIGNAL_TYPE;
-  case dbc_view_group:
-    return GROUP_TYPE;
-  }
-}
-
-export class DBCContent extends BaseControl {
+export class DBCComment extends BaseControl {
   _commentRootElement;
   _commentTextElement;
 
