dcda520228e6432b8bc7a880f3ba177f4eb109d0
Yurii Yakubin <119136621+yacubin@users.noreply.github.com>
2025-08-03 20:01:31 +0100

    Renamed ykbin into yacubin (#218)

---
+++ src/lib/Links.ts
@@ -0,0 +1,4 @@
+export const GITHUB_MAIN_URL = "https://github.com/yacubin";
+export const GITHUB_DBC_URL = `${GITHUB_MAIN_URL}/dbc`;
+export const GITHUB_LIBARCHIVE_URL = `${GITHUB_MAIN_URL}/libarchive-js`;
+export const GITHUB_WASMUX_URL = `${GITHUB_MAIN_URL}/wasmux`;
