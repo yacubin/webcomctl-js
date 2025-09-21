85e7fb794fadb29528b5b01f65d4e4043bfa2308
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-09-22 02:11:50 +0300

    LEB128Converter

--- src/control/LEB128Converter/template.ts
+++ src/control/LEB128Converter/template.ts
@@ -113,7 +113,8 @@ ${DARKMODE_SELECTOR_VALUE}
   font-family: ${IMPUT_FONT};
   font-size: 1rem;
   padding: 0.5rem;
-  border: 1px solid ${INPUT_BOR};
+  border: 2px solid;
+  border-color: ${INPUT_BOR};
   border-radius: 4px;
   border-radius: 7px;
   flex-grow: 1;
@@ -122,7 +123,7 @@ ${DARKMODE_SELECTOR_VALUE}
 .${clss.Field_Imput} > input:focus
 {
   outline: none;
-  border: 1px solid ${INPUT_BOR_FOCUS};
+  border-color: ${INPUT_BOR_FOCUS};
 }
 
 .${clss.Field_Imput} > a
