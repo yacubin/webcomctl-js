a721eadfaa4986d42946e9dd0ab661959cc9a6c6
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-09-24 20:44:43 +0300

    Utilspot Button

--- src/control/UtilspotButton/index.ts
+++ src/control/UtilspotButton/index.ts
@@ -3,18 +3,18 @@ import { ROOT_CLASS, BUTTON_ACTIVE
 // @ts-ignore
 } from "uictmplt-loader!./template.ts";
 
-export class DBCTreeInfo extends BaseControl {
+export class UtilspotButton extends BaseControl {
+  private _tableOfContents?: HTMLElement;
 
   protected _init() {
     this._tableOfContents = NQDOM.getElementByClassName(this.element, ROOT_CLASS);
-    this._buttonActive = NQDOM.getElementByClassName(this.element, BUTTON_ACTIVE);
-  }
 
-  if (this._tableOfContents) {
-    Array.prototype.forEach.call(this._tableOfContents.children, iter => {
-      if (iter.href && iter.href == location.href) {
-        iter.classList.add(this._buttonActive);
-      }
-    });
+    if (this._tableOfContents) {
+      Array.prototype.forEach.call(this._tableOfContents.children, iter => {
+        if (iter.href && iter.href == location.href) {
+          iter.classList.add(BUTTON_ACTIVE);
+        }
+      });
+    }
   }
 };
