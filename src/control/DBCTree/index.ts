b3a0bda1a0dbc04252150052936c75c82544df78
Yurii Yakubin <yurii.yakubin@gmail.com>
2025-10-04 14:55:02 +0100

    Removed old code from DBCTree

--- src/control/DBCTree/index.ts
+++ src/control/DBCTree/index.ts
@@ -2,9 +2,16 @@ import { BaseControl, NQDOM } from "webnetq-js";
 // @ts-ignore
 import { ITEM_HTML, DOCUMENT_CLASS, MESSAGE_CLASS, GROUP_CLASS, SIGNAL_CLASS, PSEUDO_CLASS, EXPAND_CLASS, CHILDFREE_CLASS, ACTIVE_CLASS, TITLE_CLASS, CHILDS_CLASS, DO_EXPAND_CLASS, DO_ACTIVE_CLASS } from "uictmplt-loader!./template.ts";
 
+const typeToClass = {
+  document: DOCUMENT_CLASS,
+  message: MESSAGE_CLASS,
+  group: GROUP_CLASS,
+  signal: SIGNAL_CLASS,
+};
+
 interface DBCNodeParams {
   name: string;
-  type: "document" | "message" | "group" | "signal";
+  type: keyof typeof typeToClass;
   selected?: boolean;
   expand?: boolean;
   pseudo?: boolean;
@@ -13,13 +20,6 @@ interface DBCNodeParams {
 };
 
 const SELECTEDCHANGED_EVENT = "selectedchanged";
-
-const typeToClass = {
-  document: DOCUMENT_CLASS,
-  message: MESSAGE_CLASS,
-  group: GROUP_CLASS,
-  signal: SIGNAL_CLASS,
-};
 
 export class DBCTree extends BaseControl {
   private _selectedElm?: HTMLElement;
@@ -34,7 +34,7 @@ export class DBCTree extends BaseControl {
     this._selectedElm = undefined;
     this.element.textContent = "";
     if (params) {
-      const root = this._createElementByNodeInfo2(params);
+      const root = this._createDbcNode(params);
       if (root) {
         if (!this._selectedElm)
           this._selectNode(root, params);
@@ -50,7 +50,7 @@ export class DBCTree extends BaseControl {
     this.dispatchEvent(SELECTEDCHANGED_EVENT, params);
   }
 
-  private _createElementByNodeInfo2(params: DBCNodeParams): HTMLElement | undefined {
+  private _createDbcNode(params: DBCNodeParams): HTMLElement | undefined {
     const typeClass = typeToClass[params.type];
     if (!typeClass || !this._itemTemplate)
       return;
@@ -88,7 +88,7 @@ export class DBCTree extends BaseControl {
       const childsElm = NQDOM.getElementByClassName(itemElm, CHILDS_CLASS);
       if (childsElm) {
         for (const iter of params.childs) {
-          const child = this._createElementByNodeInfo2(iter);
+          const child = this._createDbcNode(iter);
           child && childsElm.appendChild(child);
         }
       }
@@ -99,100 +99,5 @@ export class DBCTree extends BaseControl {
     }
 
     return itemElm;
-  }
-
-  private _createElementByNodeInfo(node: DBCNodeParams): HTMLElement | undefined {
-    const element = document.createElement("div");
-    switch (node.type) {
-    case "document":
-      element.classList.add(DOCUMENT_CLASS);
-      break;
-
-    case "message":
-      element.classList.add(MESSAGE_CLASS);
-      break;
-
-    case "group":
-      element.classList.add(GROUP_CLASS);
-      break;
-
-    case "signal":
-      element.classList.add(SIGNAL_CLASS);
-      break;
-
-    default:
-      return;
-    }
-    
-    element.classList.toggle(PSEUDO_CLASS, !!node.pseudo);
-
-    {
-      const s = document.createElement('s');
-
-      {
-        const b = document.createElement('b');
-        if (node.childs?.length) {
-          let expand = !!node.expand;
-          b.addEventListener("click", () => {
-            expand = !expand;
-            element.classList.toggle(EXPAND_CLASS, expand);
-          });
-        }
-      
-        {
-          const div = document.createElement('div');
-          b.appendChild(div);
-        }
-
-        s.appendChild(b);
-      }
-
-      {
-        const h2 = document.createElement('h2');
-
-        h2.addEventListener("click", () => {
-          if (this._selectedElm !== element)
-            this._selectNode(element, node)
-        });
-
-        {
-          const s = document.createElement('s');
-          h2.appendChild(s);
-        }
-
-        {
-          const div = document.createElement('div');
-          div.textContent = node.name;
-          h2.appendChild(div);
-        }
-
-        s.appendChild(h2);
-      }
-
-      element.appendChild(s);
-    }
-
-    
-    {
-      const span = document.createElement('span');
-
-      if (!node.childs?.length)
-        element.classList.add(CHILDFREE_CLASS);
-      else {
-        element.classList.toggle(EXPAND_CLASS, !!node.expand);
-        for (const iter of node.childs) {
-          const child = this._createElementByNodeInfo(iter);
-          child && span.appendChild(child);
-        }
-      }
-
-      element.appendChild(span);
-    }
-
-    if (node.selected) {
-      this._selectNode(element, node);
-    }
-
-    return element;
   }
 };
