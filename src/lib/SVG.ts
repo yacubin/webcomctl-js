cfb5612b650431ba4d88466a71cc69a4d37083be
Yurii Yakubin <yurii.yakubin@gmail.com>
2025-10-04 14:47:05 +0100

    Removed old SVG implementation

--- src/lib/SVG.ts
+++ src/lib/SVG.ts
@@ -1,87 +1,4 @@
-import { readFile } from 'node:fs/promises';
-import path from 'node:path';
-import { fileURLToPath } from 'url';
 import svgo from 'svgo';
-
-const plugins = [
-  { name: 'removeXMLProcInst', active: true },
-  { name: 'removeDoctype', active: true },
-  { name: 'removeComments', active: true },
-  { name: 'cleanupNumericValues', params: { floatPrecision: 2 } },
-  { name: 'convertColors', params: { names2hex: true, rgb2hex: true } },
-  { name: 'removeMetadata', active: true },
-  { name: 'removeTitle', active: true },
-  { name: 'removeDesc', active: true },
-  { name: 'removeEditorsNSData', active: true },
-  { name: 'removeUnusedNS', active: true },
-  { name: 'removeNonInheritableGroupAttrs', active: true },
-  { name: 'collapseGroups', active: true },
-];
-
-const xmlCssPlugins: any = [
-  ...plugins,
-];
-
-const htmlPlugins: any = [
-  ...plugins,
-  { name: 'removeXMLNS', active: true },
-  { name: 'removeDimensions', active: true }, // Remove width and height
-  { name: 'removeViewBox', active: false },
-];
-
-export function convertSvgToCssUrl(bytes: string) {
-  const optimizeSvg = svgo.optimize(bytes, { plugins: xmlCssPlugins });
-  
-  const svgUriData1 = 'data:image/svg+xml,' +  encodeURIComponent(optimizeSvg.data);
-  const svgUriData2 = 'data:image/svg+xml;base64,' +  Buffer.from(optimizeSvg.data).toString('base64');
-
-  const svgUriData = svgUriData1.length < svgUriData2.length ? svgUriData1 : svgUriData2;
-  return `url("${svgUriData}")`;
-}
-
-export async function loadSvgAsCssUrlAsync(metaUrl: string, filename: string) {
-  const currentFilename = fileURLToPath(metaUrl);
-  const currentDirname = path.dirname(currentFilename);
-  
-  const fname = path.resolve(currentDirname, filename);
-  const bytes = await readFile(fname, { encoding: 'utf8', flag: 'r' });
-
-  return convertSvgToCssUrl(bytes);
-}
-
-function replaceStrok(node: any, color: string) {
-  switch (node.type) {
-  case "element":
-    if (node.attributes.stroke && node.attributes.stroke !== "none")
-      node.attributes.stroke = color;
-  case "root":
-    for (const iter of node.children)
-      replaceStrok(iter, color);
-    return;
-
-  default:
-    return;
-  }
-}
-
-export function replaceAllSvgStrokeColor(bytes: string, color: string) {
-  const plugins: any = [
-    {
-      name: "replaceStroke",
-      type: "perItem",
-      fn: (node: any) => {
-        replaceStrok(node, color);
-      }
-    }
-  ];
-  const optimizeSvg = svgo.optimize(bytes, { plugins });
-  return optimizeSvg.data;
-}
-
-export function convertSvgToHtmlTag(bytes: string) {
-  const optimizeSvg = svgo.optimize(bytes, { plugins: htmlPlugins });
-  return optimizeSvg.data;
-}
 
 export interface SvgOptimizeParams {
   outputType?: "CSS-URL" | "HTML-TAG";
@@ -141,4 +58,12 @@ export function createSvgOptimize(input: string, params: SvgOptimizeParams): str
   }
 
   return result;
+}
+
+export function convertSvgToCssUrl(input: string) {
+  return createSvgOptimize(input, { outputType: "CSS-URL" });
+}
+
+export function convertSvgToHtmlTag(input: string) {
+  return createSvgOptimize(input, { outputType: "HTML-TAG" });
 }
