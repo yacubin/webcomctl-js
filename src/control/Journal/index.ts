3d55959a333566fe4c3f6279e4ff683cebb4b4f9
Yurii Yakubin <yurii.yakubin@gmail.com>
2025-10-07 19:14:34 +0100

    Journal script implementation

--- src/control/Journal/index.ts
+++ src/control/Journal/index.ts
@@ -1,7 +1,29 @@
-import { BaseControl } from "webnetq-js";
+import { BaseControl, NQDOM } from "webnetq-js";
+// @ts-ignore
+import { TITLE, LIST } from "uictmplt-loader!./template.ts";
 
 export class Journal extends BaseControl {
+  private _titleElm?: HTMLElement;
+  private _dataElm?: HTMLElement;
+
   protected _init() {
+    this._titleElm = NQDOM.getElementByClassName(this.element, TITLE) as HTMLElement;
+    this._dataElm = NQDOM.getElementByClassName(this.element, LIST) as HTMLElement;
+  }
+
+  public setTitle(title: string) {
+    this._titleElm && (this._titleElm.textContent = title);
+  }
+
+  public setData(lines: string[]) {
+    if (!this._dataElm)
+      return;
+
+    this._dataElm.innerHTML = "";
+    for (const text of lines) {
+      const element = document.createElement("div");
+      element.textContent = text;
+      this._dataElm.appendChild(element);
+    }
   }
 };
-
