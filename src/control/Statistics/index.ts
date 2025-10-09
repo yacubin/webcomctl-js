9c531fa33c6f4c1c1fcf6b5c1b05c78b3e08617c
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-10-09 19:08:42 +0300

    Add UtilspotStatistic (#252)

---
+++ src/control/Statistics/index.ts
@@ -0,0 +1,8 @@
+import { BaseControl, NQDOM } from "webnetq-js";
+// @ts-ignore
+import {  } from "uictmplt-loader!./template.ts";
+
+export class Statistics extends BaseControl {
+   _init() {
+  }
+};
