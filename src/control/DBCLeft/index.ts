e576520e2c5e401153b2535451127603e86fc04a
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-11-07 01:51:09 +0200

    creation DBCLeftPanelBlock and DBCSplitte

--- src/control/DBCLeft/index.ts
+++ src/control/DBCLeft/index.ts
@@ -3,9 +3,10 @@ import { BaseControl, NQDOM } from "webnetq-js";
 import { ROOT_HTML } from "uictmplt-loader!./template.ts";
 
 export class DBCLeft extends BaseControl {
-    protected _init() {
-  }
   public static createElement(document: HTMLDocument): HTMLElement {
     return NQDOM.createElement(ROOT_HTML, document) as HTMLElement;
+  }
+  
+  protected _init() {
   }
 };
