f8b3ce2b1a308b5dd5c7b7a139e228c29beaaace
Yurii Yakubin <yurii.yakubin@gmail.com>
2025-11-08 22:45:45 +0000

    DMBtn inherits DarkModeButton

--- src/control/DMBtn/index.ts
+++ src/control/DMBtn/index.ts
@@ -1,15 +1,8 @@
-import { BaseControl, NQDOM, Setting } from "webnetq-js";
 // @ts-ignore
 import { ROOT_CLASS, TOGGLE_CLASS } from "uictmplt-loader!./template.ts";
+import { DarkModeButton } from "@/comp/DarkModeButton/control";
 
-const kDarkModeTip = "Toggle dark mode";
-const kLightModeTip = "Toggle light mode";
-
-const THEMCHANGE_EVENT = "themchange";
-
-export class DMBtn extends BaseControl {
-  private _toggleElm?: HTMLElement;
-
+export class DMBtn extends DarkModeButton {
   public static createElement(document: HTMLDocument): HTMLElement {
     // <div class="${ROOT_CLASS}">
     //   <span class="${TOGGLE_CLASS}"></span>
@@ -22,28 +15,5 @@ export class DMBtn extends BaseControl {
 
     element.appendChild(toggleElm);
     return element;
-  }
-
-  protected _init() {
-    this._toggleElm = NQDOM.getElementByClassName(this.element, TOGGLE_CLASS);
-    this._toggleElm && this._toggleElm.addEventListener("click", (event) => {
-      const setting = Setting.getInstance();
-      setting.toggleTheme();
-      this._setTheme(setting.getTheme());
-    });
-
-    const setting = Setting.getInstance();
-    this._setTheme(setting.getTheme());
-
-    setting.addEventListener(THEMCHANGE_EVENT, event => this._setTheme(event.theme));
-  }
-
-  private _setTheme(theme: string) {
-    const document = this.element.ownerDocument;
-    if (document.documentElement)
-      document.documentElement.dataset.theme = theme;
-    if (this._toggleElm) {
-      this._toggleElm.setAttribute("title", (theme == Setting.DARK_VAL) ? kDarkModeTip : kLightModeTip);
-    }
   }
 };
