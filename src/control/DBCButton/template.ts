2026eefab5764acc855bc661bcca170c7fd6cce0
Serhey Yakubin <yakubin.serhey@gmail.com>
2025-11-09 09:47:01 +0200

    new parts

--- src/control/DBCButton/template.ts
+++ src/control/DBCButton/template.ts
@@ -1,7 +1,6 @@
 import ControlMaker from "@/lib/ControlMaker";
 import { convertSvgToCssUrl } from "@/lib/SVG";
 import { DARKMODE_SELECTOR_VALUE } from "@/lib/DarkMode";
-import { TOOLBAR_DBC_FONT_SANS } from "@/lib/WickedTheme";
 
 import leftButSvg from "./left-but.svg";
 import leftButHoverSvg from "./left-but-hover.svg";
@@ -16,12 +15,18 @@ const clss = mk.newClassNameMap([
 const left_but_img = convertSvgToCssUrl(leftButSvg);
 const left_but_hov_img = convertSvgToCssUrl(leftButHoverSvg);
 
+const button_bg = '#dddddd40';
+
 const vars = mk.newCSSVariableMap({
 });
 
 mk.newHTML("ROOT_HTML", `
 
-  <div class="${clss.ROOT_CLASS} ${clss.PORT_CLASS}"></div>
+   <s class="${clss.ROOT_CLASS}>
+    <div>
+      <div class="dbc_bn_nav"></div>
+    </div>
+  </s>
 `);
 
 mk.newCSS('CSS', `
@@ -35,6 +40,38 @@ ${DARKMODE_SELECTOR_VALUE}
   ${vars.toString(1)};
 }
 
+.${clss.ROOT_CLASS}
+{
+  grid-area: header;
+  display: flex;
+  height: 50px;
+  width: calc(100% - 20px);
+  align-items: center;
+  background: var(--left-grad);
+  z-index: 2;
+}
+
+.${clss.ROOT_CLASS} > div > div
+{
+  width: 20px;
+  height: 20px;
+  background-image: ${left_but_img};
+  background-size: contain;
+  background-origin: padding-box;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.${clss.ROOT_CLASS} > div:hover
+{
+  background-color: ${button_bg};
+}
+
+.${clss.ROOT_CLASS} > div:hover > div
+{
+  background-image: ${left_but_hov_img};
+  transition: background-image 0.5s;
+}
 `);
 
 export function buildComponent()
